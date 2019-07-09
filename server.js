cd13e1db4fa2c17f5c3f9cf4ca9463103cbae8db
endy <endyaduba@gmail.com>
2019-07-09 10:27:21 +0100

    added mongodb connection

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT;
+var port = process.env.PORT || 8080;
 var morgan = require('morgan');
+var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost:27017/meanstackapp' ,{ useNewUrlParser: true }, (err)=>{
+    if(err){
+        console.log("not connected to database");
+    }
+    else{
+        console.log("Successfully Connected to Mongodb");
+}}) 
 app.get('/', (req,res) => res.send('hello from the other side'))
 
 app.listen(port || 8080, (req,res)=>
-    console.log("listening on port" + 8080)
+    console.log("listening on port" + port)
 );
