478603fc5f439d5bc64e20f1bfae9130a74b98c9
endy <endyaduba@gmail.com>
2019-09-24 22:51:08 +0100

    setting up the paths for anugular usage

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var router = express.Router();
 var appRoutes = require('./app/routes/api')(router);
+var path = require('path');
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/api',appRoutes);
+app.use(express.static(__dirname + '/public'));
 
 mongoose.set('useCreateIndex', true);
 mongoose.connect('mongodb://localhost:27017/meanstackapp' ,{ useNewUrlParser: true }, (err)=>{
@@ -21,7 +23,7 @@ mongoose.connect('mongodb://localhost:27017/meanstackapp' ,{ useNewUrlParser: tr
         console.log("Successfully Connected to Mongodb");
 }}) 
 
-app.get('/', (req,res) => res.send('hello from the other side'))
+app.get('*', (req,res) => res.sendFile(path.join(__dirname + '/public/app/views/index.html')));
 
 
 app.listen(port, (req,res)=>
